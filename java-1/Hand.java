8fff65d775cd9606595289ae021d19216c8a5cf2
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-05-12 23:30:14 +0200

    first error

---
+++ java-1/Hand.java
@@ -0,0 +1,39 @@
+import java.util.Vector;
+
+public class Hand {
+
+   private Vector hand;   // The cards in the hand.
+   
+   public Hand() {
+           // Create a Hand object that is initially empty.
+      hand = new Vector();
+   }
+   
+   public void addCard(Card c) {
+
+   }
+   
+   public void removeCard(Card c) {
+
+   }
+   
+   public void removeCard(int position) {
+
+   }
+   
+   
+   public Card getCard(int position) {
+      Card myCard = new Card(1,1);
+      return myCard;
+
+   }
+   
+   public void sortBySuit() {
+
+   }
+   
+   public void sortByValue() {
+
+   }
+   
+}
